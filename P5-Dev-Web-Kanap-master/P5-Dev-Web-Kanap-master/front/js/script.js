9e3e4689a76a560ae0173baac1eede505f6b4b0b
sarahvar <sarahvarin95@gmail.com>
2022-11-19 23:10:50 +0000

    fetch

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,14 +1,3 @@
 fetch("http://localhost:3000/api/products")
-    .then(function(res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then(function(products) {
-        displayProducts(products);
-    })
-    .catch(function(err) {
-        // Retourner si il y a erreur
-    });
-console.log("test")
-
+    .then((res) => res.json())
+    .then((data) => console.log(data))
