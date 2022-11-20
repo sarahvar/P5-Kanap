6b03ec77330022d927d05df290d240d7478cd48e
sarahvar <sarahvarin95@gmail.com>
2022-11-20 19:55:12 +0000

    product

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,42 +1,26 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => {
-        const imageUrl = data[0].imageUrl
+    .then((data) => addProducts(data))
+    
+        
  //L'API Fetch récupere les données des produits sur le server 3000
 
-    const anchor = document.createElement("a")
-    anchor.href = imageUrl
-    anchor.text = "Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu."
-    const items = document.querySelector("#items")
-    if (items != null) {
-    items.appendChild(anchor)
-    }
 
-
-});
-
-function addProducts(product){
-    const productList = document.querySelector("#product-list")
-    if (productList != null) {
-        const productElement = document.querySelector("div")
-        productElement.classList.add("product")
-        productElement.innerHTML = `
-        <div class="product-image">
-        <img src="${product.imageUrl}" alt="${product.name}">
-        </div>
-        <div class="product-name">
-        ${product.name}
-        </div>
-        <div class="product-price">
-        ${product.price} €
-        </div>
-        <div class "product-action">
-        <button class="btn btn-primary" data-id="${product.id}">Ajouter au panier<button>
-        </div>
-        `
-        productList.appendChild(productElement)
-    }
+function addProducts(donnees){
+    const imageUrl = donnees[0].imageUrl
+    const anchor = makeAnchor(imageUrl);
+    appendChildren(anchor)
 }
 
+function makeAnchor(url){
+    const anchor = document.createElement("a")
+    anchor.href = url
+    return anchor
+}
 
-//Si items est different de null on donne l'enfant anchor à items
+function appendChildren(anchor){
+    const items = document.querySelector("#items")
+    if (items != null) {
+        items.appendChild(anchor)
+    }
+}
