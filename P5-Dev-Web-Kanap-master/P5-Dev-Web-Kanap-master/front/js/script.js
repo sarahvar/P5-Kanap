2ec9cf6d8a5a70513c1f1b45e1a37ad99c1841ea
sarahvar <sarahvarin95@gmail.com>
2022-11-20 00:03:45 +0000

    ancre

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,3 +1,7 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((data) => console.log(data))
+ //L'API Fetch récupere les données des produits sur le server 3000
+
+const anchor = document.createElement("a")
+anchor.href = "http://localhost:3000/images/kanap01.jpeg"
