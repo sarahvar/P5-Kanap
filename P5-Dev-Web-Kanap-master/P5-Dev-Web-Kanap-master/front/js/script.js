4ef1e52efe4d5fb3f726f6fc39683df9089c155e
sarahvar <sarahvarin95@gmail.com>
2022-11-20 18:23:46 +0000

    imageUrl

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,14 +1,22 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        console.log(data);
+        const imageUrl = data[0].imageUrl
+        console.log("url de l'image", imageUrl)
  //L'API Fetch récupere les données des produits sur le server 3000
 
-const anchor = document.createElement("a")
-anchor.href = "http://localhost:3000/images/kanap01.jpeg"
-anchor.text = "Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu."
-
-const items = document.querySelector("#items")
-if (items != null) {
+    const anchor = document.createElement("a")
+    anchor.href = imageUrl
+    anchor.text = "Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu."
+    const items = document.querySelector("#items")
+    if (items != null) {
     items.appendChild(anchor)
-}
+    console.log("nous avons bien ajouter le lien")
+    }
+});
+
+
+
+
 //Si items est different de null on donne l'enfant anchor à items
