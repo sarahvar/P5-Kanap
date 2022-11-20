030abe7f26118a0decde1f366daef6f8db249cd5
sarahvar <sarahvarin95@gmail.com>
2022-11-20 20:10:02 +0000

    make

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,20 +1,20 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((data) => addProducts(data))
-    
         
  //L'API Fetch récupere les données des produits sur le server 3000
 
 
 function addProducts(donnees){
-    const imageUrl = donnees[0].imageUrl
-    const anchor = makeAnchor(imageUrl);
+    const id = donnees[0]._id
+    const anchor = makeAnchor(id);
+    const article = makeArticle()
     appendChildren(anchor)
 }
 
-function makeAnchor(url){
+function makeAnchor(id){
     const anchor = document.createElement("a")
-    anchor.href = url
+    anchor.href = "./product.html?id=" + id
     return anchor
 }
 
@@ -23,4 +23,24 @@ function appendChildren(anchor){
     if (items != null) {
         items.appendChild(anchor)
     }
+}
+function makeArticle(){
+const article = document.createElement("article")
+console.log(article)
+return arcticle
+}
+
+function makeImage(imageUrl, altTxt){
+const image =  document.createElement("img")
+image.src = imageUrl
+image.alt = altTxt
+return image
+}
+
+function makeH3(){
+
+}
+
+function makeParagraph(){
+
 }
