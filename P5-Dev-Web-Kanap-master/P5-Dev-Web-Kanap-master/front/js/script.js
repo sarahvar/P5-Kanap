e6a20097872d03b1c708bc373f6ef8598d50db48
sarahvar <sarahvarin95@gmail.com>
2022-11-25 14:22:48 +0000

    catch error

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,8 +1,8 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((data) => addProducts(data))
-    .catch(() => {
-        console.log("Une erreur est survenue");   
+    .catch((error) => {
+        console.log(error);   
     });
     
 
