3f885c56cf16627cc4a9c971a5a870c5da77df89
sarahvar <sarahvarin95@gmail.com>
2022-11-26 21:22:27 +0000

    fetch

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/product.js
@@ -2,3 +2,8 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
 const productId = urlParams.get("_id")
 console.log({queryString})
+
+fetch(`http://localhost:3000/api/products/`+_id)
+	.then((reponse) => {
+		if (reponse.ok) return reponse.json();
+	})
