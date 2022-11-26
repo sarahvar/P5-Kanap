ef6104d569bda51b03e3819a9139b92be2e9cf35
sarahvar <sarahvarin95@gmail.com>
2022-11-26 23:09:11 +0000

    .item_img

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/product.js
@@ -8,20 +8,13 @@ fetch(`http://localhost:3000/api/products/${_id}`)
     .then((res) => handleData(res))
     
 function handleData(kanap){
-    const altTxt = kanap.altTxt
-    const colors = kanap.colors
-    const description = kanap.description
-    const imageUrl = kanap.imageUrl
-    const name = kanap.name
-    const price = kanap.price
-    const _id = kanap._id
+    const { altTxt, colors, description, imageUrl, name, price, _id} = kanap
     makeImage(imageUrl, altTxt)
 }
-
 function makeImage(imageUrl, altTxt){
-    const image = document.createElement("image")
+    const image = document.createElement("img")
     image.src = imageUrl
     image.alt = altTxt
-    const parent = document.querySelector("item__img")
-    parent.appendChild(image)
+    const parent = document.querySelector(".item__img")
+    if (parent != null) parent.appendChild(image)
 }
