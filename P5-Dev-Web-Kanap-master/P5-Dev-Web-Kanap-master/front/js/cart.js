e333f9e17e9803749983d4b7e47d8efaf2b529c5
sarahvar <sarahvarin95@gmail.com>
2022-11-27 22:59:05 +0000

    position

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -1,2 +1,6 @@
 const numberOfItems = localStorage.length;
-console.log("Nombre d'objets ajoutés:", numberOfItems)
+
+for (let i = 0; i < numberOfItems; i++){
+    const item = localStorage.getItem(localStorage.key(i))
+    console.log("objet à la position", i, "est",item)
+}
