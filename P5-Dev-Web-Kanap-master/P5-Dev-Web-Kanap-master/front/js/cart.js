e0ff48c5111c08e44cc6cfd0aa02e87a96ecd2e7
sarahvar <sarahvarin95@gmail.com>
2022-11-27 23:45:00 +0000

    function

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -1,6 +1,15 @@
 const cart = [];
 
 retrieveItemsFromCache()
+console.log(cart)
+cart.forEach((item) => displayItem(item))
+
+//altTxt: "Photo d'un canapé bleu, deux places"
+//color: "White"
+//imageUrl: "http://localhost:3000/images/kanap01.jpeg"
+//price: 1849
+//quantity: 3
+//_id: "107fb5b75607497b96722bda5b504926"
 
 function retrieveItemsFromCache(){
     const numberOfItems = localStorage.length;
@@ -10,4 +19,13 @@ for (let i = 0; i < numberOfItems; i++){
     cart.push(itemObject)
 }
 }
-console.log(cart)
+
+function displayItem(item){
+const image = makeImage(item)
+}
+function makeImage(item){
+    const image = document.createElement('img')
+    image.src = item.imageUrl
+    image.alt = item.altTxt
+    return image
+}
