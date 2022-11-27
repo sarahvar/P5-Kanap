80332de11635b658f3a384e2be5aa673333d25c6
sarahvar <sarahvarin95@gmail.com>
2022-11-27 23:27:51 +0000

    function

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -1,10 +1,13 @@
-const numberOfItems = localStorage.length;
-console.log(numberOfItems)
 const cart = [];
 
+retrieveItemsFromCache()
+
+function retrieveItemsFromCache(){
+    const numberOfItems = localStorage.length;
 for (let i = 0; i < numberOfItems; i++){
     const item = localStorage.getItem(localStorage.key(i))
     const itemObject = JSON.parse(item)
     cart.push(itemObject)
+}
 }
 console.log(cart)
