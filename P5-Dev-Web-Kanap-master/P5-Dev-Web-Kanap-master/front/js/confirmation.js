ea456b435f7cb48ab6c0e5b04fe75322e10bd65c
sarahvar <sarahvarin95@gmail.com>
2022-12-11 23:16:40 +0000

    insertion_orderId_page

--- P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/confirmation.js
+++ P5-Dev-Web-Kanap-master/P5-Dev-Web-Kanap-master/front/js/confirmation.js
@@ -1,8 +1,18 @@
 // Récupération du "orderId" dans l'URL
 
-const queryString = window.location.search
-const urlParams = new URLSearchParams(queryString)
-const orderId = urlParams.get("orderId")
-console.log(orderId)
+let orderId = getOrderId()
+displayOrderId(orderId)
+
+function getOrderId(){
+    let queryString = window.location.search
+    let urlParams = new URLSearchParams(queryString)
+    return urlParams.get("orderId")
+}
 
 
+// Insertion de l'orderId sur la page confirmation
+
+function displayOrderId(orderId){
+    let orderIdElement = document.getElementById("orderId")
+    orderIdElement.textContent = orderId
+}
